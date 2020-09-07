deeaa9a14c8e4ca727453b5ab65cae5ff58e4d42
Seth <colts18seth@gmail.com>
2020-09-07 13:13:00 -0400

    finished exercise part 1

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Card.css';
 
-function Card({ img }) {
+function Card({ img, rotation, height }) {
+    const styles = {
+        transform: rotation,
+        margin: height
+    }
+
     return (
-        <img className="Card" src={img} alt="Playing Card" >
+        <img className="Card" style={styles} src={img} alt="Playing Card" >
         </img >
     );
 }
--- src/CardPile.js
+++ src/CardPile.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import { v4 as uuid } from 'uuid';
 import Card from './Card'
-// import './CardPile.css';
+import './CardPile.css';
 
 function CardPile() {
     const deckId = useRef();
@@ -15,21 +16,42 @@ function CardPile() {
         getNewDeck()
     }, [])
 
+    const cardRotation = () => {
+        const randNum = (Math.random() * 80) - 40;
+        return ` rotate(${randNum}deg)`
+    }
+
+    const cardHeight = () => {
+        const randNum = (Math.random() * 30) - 15;
+        return `${randNum}px`
+    }
+
     const drawCard = async () => {
         const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId.current}/draw/`);
-        setCards(res.data.cards)
+        setCards([...cards, {
+            image: res.data.cards[0].image,
+            rotation: cardRotation(),
+            height: cardHeight(),
+            key: uuid()
+        }])
         setRemaining(res.data.remaining)
     }
 
     return (
         <div className="CardPile">
-            <h3>Cards Remaining In Deck: {remaining ? remaining : alert('Error: no cards remaining!')}</h3>
-            <button onClick={drawCard}>Draw a Card</button>
+
+            <h3>Cards Remaining In Deck: {remaining >= 0 ? remaining : alert('Error: no cards remaining!')}</h3>
+
+            {remaining ?
+                <button onClick={drawCard}>Draw a Card</button> : ""
+            }
+
             <div className="Card">
                 {cards.map(card => (
-                    <Card img={card.image} />
+                    <Card img={card.image} rotation={card.rotation} height={card.height} key={card.key} />
                 ))}
             </div>
+
         </div>
     );
 }
